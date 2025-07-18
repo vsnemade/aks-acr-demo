3833eaef381455a53252a157c8e053c849289ba4
Vishal Nemade <vsnemade@gmail.com>
2025-07-18 23:55:54 +0530

    terraform code

---
+++ terraform/aks-acr-demo/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "demoaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+}
---
+++ terraform/aks-acr-demo/variables.tf
@@ -0,0 +1,4 @@
+variable "location"      { default = "eastus" }
+variable "resource_group" { default = "rg-aks-demo" }
+variable "aks_name"       { default = "aks-demo-cluster" }
+variable "acr_name"       { default = "acrdemoregistryvish" }
