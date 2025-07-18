67b48321c0857ecc2afb883ac36c138c72ae9aba
Vishal Nemade <vsnemade@gmail.com>
2025-07-18 23:55:54 +0530

    terraform code

---
+++ terraform/aks-acr-demo/output.tf
@@ -0,0 +1,8 @@
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
